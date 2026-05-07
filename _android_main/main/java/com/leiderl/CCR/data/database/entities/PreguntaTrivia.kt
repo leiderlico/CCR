0acce5fc1296ab990afde46939bb0f41d18226bc
leiderlico <leiderlicona1234@gmail.com>
2026-05-07 12:08:51 -0500

    Z

--- _android_main/main/java/com/leiderl/CCR/data/database/entities/PreguntaTrivia.kt
+++
@@ -1,8 +0,0 @@
-package com.leiderl.CCR.data.database.entities
-
-data class PreguntaTrivia(
-    val id: String,
-    val pregunta: String,
-    val correcta: String,
-    val opciones: List<String>   // ya barajadas por el script
-)
